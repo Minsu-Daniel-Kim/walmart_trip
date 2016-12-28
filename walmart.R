4528b0c60d474d7b4fb525ca2d20699c139090d6
Brian Lee <Brian Lee>
2016-12-28 15:00:51 -0800

    isolating type 25

--- walmart.R
+++ walmart.R
@@ -1,13 +1,24 @@
 library(ggplot2)
 library(data.table)
+library(dplyr)
 
-train <- read.csv("train.csv")
+train <- fread("train.csv")
 str(train)
 
+train$Weekday <- factor(train$Weekday, levels = c("Monday", "Tuesday", "Wednesday",
+                                                  "Thursday", "Friday", "Saturday", "Sunday"))
+
+
+# Distribution of TripType, Weekday, ScanCount, and DepartmentDescription
 ggplot(train, aes(x = as.factor(TripType))) + geom_bar()
 ggplot(train, aes(x = as.factor(Weekday))) + geom_bar()
 ggplot(train, aes(x = as.factor(ScanCount))) + geom_bar()
 ggplot(train, aes(x = as.factor(DepartmentDescription))) + geom_bar()
+
+
+train %>% 
+  filter(TripType == 40) %>%
+  ggplot(aes(x = Weekday)) + geom_bar()
 
 ggplot(train, aes(x = Weekday, y = as.factor(TripType), colour = Weekday)) + geom_point()
 ggplot(train, aes(x = as.factor(ScanCount), y = as.factor(TripType))) + geom_jitter()
